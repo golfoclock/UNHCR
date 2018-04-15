efe234522ce07b5d6aabd99f9e9e9bc05eafcc9c
golfoclock <cmmc@protonmail.ch>
2018-04-15 17:31:45 -0400

    redefined refugee bar chart

--- static/lib/js/graph.js
+++ static/lib/js/graph.js
@@ -1,16 +1,12 @@
-
 queue()
-   .defer(d3.json, "/unitedNations/refugeesReport")
-   .await(makeGraphs);
+    .defer(d3.json, "/unitedNations/refugeesReport")
+    .await(makeGraphs);
 
 function makeGraphs(error, projectsJson) {
 
     //Clean projectsJson data
     var Asylum_Seekers = projectsJson;
     var dateFormat = d3.time.format("%Y").parse;
-
-
-
 
 
     // var maxCountry = totalRefugee.top(1)[0].value;
@@ -50,33 +46,29 @@ function makeGraphs(error, projectsJson) {
     });
 
 
-    originDim.filter("Syria")
-    var syrianRefugees = yearDim.group().reduceSum(function (d) {
-        return d['TotalPopulation']
+    var Refugees = yearDim.group().reduceSum(function (d) {
+        return d['Refugees'];
 
     });
-
-
-
 
 
 //LIST OF CHARTS - DON'T FORGET TO CHANGE HTML
     var originChart = dc.rowChart("#origin-chart");
     var countryChart = dc.pieChart("#country-chart");
-    var syriaChart = dc.barChart("#syria-chart");
+    var refugeeBarChart = dc.barChart("#refugee-chart");
     var totalPeople = dc.numberDisplay("#number-box");
 
 
     //WHAT QUESTION DOES YOUR CHART ANSWER??
 
     countryChart
-        .height(220)
-        .radius(90)
-        .innerRadius(0)
+        .height(250)
+        .radius(100)
+        .innerRadius(40)
         .transitionDuration(1500)
         .dimension(countryDim)
         .group(totalRefugee)
-        d3.scale.category10();
+    d3.scale.category10();
 
 
     originChart
@@ -88,31 +80,35 @@ function makeGraphs(error, projectsJson) {
         // .xAxisLabel("Number of Persons")
         // .yAxisLabel("Country")
         .xAxis().ticks(10).tickFormat(d3.format("s"))
-        d3.scale.category10()(1);
-    originChart.ordering(function (d) { return -d.value})
+    d3.scale.category10()(1);
+    originChart.ordering(function (d) {
+        return -d.value
+    })
     originChart.rowsCap([10])
     originChart.othersGrouper(false);
 
 
-    syriaChart
+    refugeeBarChart
         .width(1300)
-       .height(500)
-       .margins({top: 10, right: 75, bottom: 30, left: 75})
-       .dimension(yearDim)
-       .group(syrianRefugees)
-       .transitionDuration(500)
+        .height(500)
+        .margins({top: 10, right: 75, bottom: 30, left: 75})
+        .dimension(yearDim)
+        .group(Refugees)
+        .transitionDuration(500)
         .elasticY(true)
-       .xAxisLabel("Year")
-        .yAxisLabel("Number of Syrians")
-       .yAxis().ticks(5)
-    syriaChart.xAxis().tickFormat(d3.format('d'));
-    syriaChart.x(d3.scale.linear().domain([2005,2015]));
-
+        .xAxisLabel("Year")
+        .yAxisLabel("Number of Refugees")
+        .yAxis().ticks(10).tickFormat(d3.format("s"))
+    // .yAxis().ticks(5)
+    refugeeBarChart.xAxis().tickFormat(d3.format('d'))
+    refugeeBarChart.x(d3.scale.linear().domain([2005, 2015]));
 
 
     totalPeople
         .formatNumber(d3.format(","))
-        .valueAccessor( function(d) { return d.value })
+        .valueAccessor(function (d) {
+            return d.value
+        })
         .group(totalRefugee);
 
 
